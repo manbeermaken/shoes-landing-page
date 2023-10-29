6ba3cd8fedb0247a0c8cf9f57e9f1d1811e1fa86
Manbeer Maken <manbeersingh1300@gmail.com>
2023-10-29 11:12:19 +0000

    add button to hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import Button from "../components/Button";
 import {arrowRight} from '../assets/icons'
-
+import {statistics} from '../constants'
 const Hero = () => {
   return (
     <section
@@ -8,7 +8,7 @@ const Hero = () => {
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 ">
-        <p>Our Summer Collections</p>
+        <p >Our Summer Collections</p>
         <h1>
           <span>The New Arrival</span>
           <br />
@@ -20,6 +20,14 @@ const Hero = () => {
           your active life.
         </p>
         <Button label="Shop Now" iconURL={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+            {statistics.map((stat)=>(
+              <div key={stat.label}>
+                <p>{stat.value}</p>
+                <p>{stat.label}</p>
+              </div>
+            ))}
+        </div>
       </div>
     </section>
   );
