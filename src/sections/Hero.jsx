28894a66b4eab2413788ee9c540792d166c49609
Manbeer Maken <manbeersingh1300@gmail.com>
2023-10-29 08:00:59 +0000

    add button and content on hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,14 +1,28 @@
+import Button from "../components/Button";
+import {arrowRight} from '../assets/icons'
+
 const Hero = () => {
   return (
     <section
-    id="home"
-    className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full">
-
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 ">
+        <p>Our Summer Collections</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike </span>
+          Shoes
+        </h1>
+        <p className="">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="Shop Now" iconURL={arrowRight} />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
